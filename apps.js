982160ef0b96900a7aae90fc58da5c336a8049de
Muhammad Hamza <143834218+hhamza56@users.noreply.github.com>
2023-09-15 09:36:27 +0100

    complete

---
+++ apps.js
@@ -0,0 +1,85 @@
+// var arr = ["appale", "mangoo", "orange", "sami", "fareed", "haris"];
+
+// document.write(
+
+//     "<select>" + "<option>" + arr[0] + "</option>" +
+//     "<option>" + arr[1] + "</option>" +
+//     "<option>" + arr[2] + "</option>" +
+//     "<option>" + arr[3] + "</option>" +
+//     "<option>" + arr[4] + "</option>" +
+//     "<option>" + arr[5] + "</option>"
+//     + "</select>"
+// )
+
+
+
+// var arr = ["appale", "mango","sami","fareeed"]
+
+// console.log("orignal arry ", arr);
+// var startingvalue = +prompt("enter starting value");
+
+// var endingvalue = +prompt("enter ending value");
+
+// var copy = arr.slice (startingvalue, endingvalue)
+
+// console.log("uptade arry", copy)
+
+
+
+
+// var userinput = +prompt("enter yout number")
+// var userlantgh = +prompt("enter yout number")
+
+
+
+// for (var k = 1; k <= length; k++) {
+//     document.write(k + "welcome user" + "<br>")
+//     console.log("welcome user" + k)
+// }
+
+
+
+
+// var usercity = prompt("were do you live");
+// var arr = ["lahore", "islamabad", "karachi", "multan"];
+// for (var i = 0; i <=arr.length; i++){
+//     if (usercity === "karachi") {
+//         alert("it's is clean city")
+//     } else alert("this rong ")
+// }
+
+
+// // var multipale = +prompt("Enter your Number")
+
+// // var lenth = +prompt("enter multipale Table")
+
+
+// // for (var i = 1; i <= lenth; i++) {
+// //     document.write(multipale + "x" + i + "=" + multipale * i + "<br>")
+// }
+
+
+// var fruit = ["appale", "bnana", "mago", "orange", "strawberry"]
+// for (var i = 0; i <fruit.length; i++) {
+//     document.write("Element at index  example" + i + "is" + fruit [i] + "<br>")
+// }
+
+
+// for (var i = 1; i <= 10; i++) {
+//     document.write(i + "<br>")
+// }
+
+
+// for (var i = 10; i >= 1; i--) {
+//     document.write(i  + "<br>")
+// }
+
+
+
+
+
+
+
+
+
+
